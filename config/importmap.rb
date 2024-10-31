d12e2db13907738d5ed324d07540c68d4237640a
Ryuta0210 <ryutoyoda0210@gmail.com>
2024-10-31 16:14:02 +0900

    Project_new画面の完成

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "menu", to:"menu.js"
 pin "step", to:"step.js"
+pin "step2", to:"step2.js"
+pin "step3", to:"step3.js"
 
